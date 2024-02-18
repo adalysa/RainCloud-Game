2db0a26011abf745f88aab7e16f686de0161353c
Devin Rosales <devinrosales@gmail.com>
2024-02-18 08:54:38 -0600

    fix: adding sprite.cs

--- RainCloud/CloudMovement.cs
+++ RainCloud/CloudMovement.cs
@@ -3,7 +3,7 @@ using Microsoft.Xna.Framework.Input;
 using System;
 using System.Diagnostics;
 
-namespace RainCloud2
+namespace PlayerMovement
 {
     public class CloudMovement
     {
--- RainCloud/Game1.cs
+++ RainCloud/Game1.cs
@@ -3,7 +3,9 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Diagnostics;
 using System.Threading;
-using RainCloud2;
+using PlayerMovement;
+using System.Collections.Generic;
+using RainCloud.Sprites;
 
 namespace RainCloud
 {
@@ -16,6 +18,9 @@ namespace RainCloud
 
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        private List<Sprite> _sprites;
+
 
         public Game1()
         {
@@ -50,6 +55,22 @@ namespace RainCloud
             // TODO: use this.Content to load your game content here
 
             cloudTexture = Content.Load<Texture2D>("cloud1");
+
+
+            _sprites = new List<Sprite>()
+            {
+                new Cloud(cloudTexture)
+                {
+                cloudPosition,  //Position
+                null,           //Source Rectangle 
+                Color.White,    //Color
+                0f,           //Rotation
+                new Vector2(cloudTexture.Width / 2, cloudTexture.Height / 2),   //Origin
+                cloudSize,      //scale 0 - 1f
+                SpriteEffects.None,     //Depth
+		        .1f
+                }
+             };
         }
 
         protected override void Update(GameTime gameTime)
@@ -64,18 +85,20 @@ namespace RainCloud
             cloudPosition = CloudMovement.KeyCloudMovement(cloudPosition, cloudSpeed, gameTime);
 
             if (cloudSize <= 0.0)
-            {
+                {
                 Debug.WriteLine("GAME OVER!");
-            }
+                }
 
             if (cloudSize <= 0.0)
-            {
+                {
                 Debug.WriteLine("GAME OVER!");
-            }
+                }
 
             base.Update(gameTime);
 
         }
+
+
 
         protected override void Draw(GameTime gameTime)
         {
@@ -89,12 +112,14 @@ namespace RainCloud
 
             _spriteBatch.Begin();
 
+
+
             //Rectangle # meanings: Rectangle(int x, int y, int width, int height)
             //Rectangle cloudPosition = new Rectangle(100, 100, 200, 100);
 
             //_spriteBatch.Draw(cloudTexture, cloudPosition, Color.White);
             //Vector2 cloudPosition = new Vector2(800, 100);
-
+            /*
             _spriteBatch.Draw(
                 cloudTexture,   //Texture 
                 cloudPosition,  //Position
@@ -104,7 +129,7 @@ namespace RainCloud
                 new Vector2(cloudTexture.Width / 2, cloudTexture.Height / 2),   //Origin
                 cloudSize,      //scale 0 - 1f
                 SpriteEffects.None,     //Depth
-		.1f
+		        .1f
             );
 
             _spriteBatch.Draw(
@@ -130,11 +155,51 @@ namespace RainCloud
                 SpriteEffects.None,
                 .1f
              );
+            */
 
+            foreach (var sprite in _sprites)
+                sprite.Draw(gameTime, spriteBatch);
 
             _spriteBatch.End();
 
             base.Draw(gameTime);
         }
+        protected void PostUpdate(GameTime gameTime)
+        {
+            // 1. Check collision between all current "Sprites"
+            // 2. Add "Children" to the list of "_sprites" and clear
+            // 3. Remove all "IsRemoved" sprites
+
+            foreach (var spriteA in _sprites)
+            {
+                foreach (var spriteB in _sprites)
+                {
+                    if (spriteA == spriteB)
+                        continue;
+
+                    if (spriteA.Intersects(spriteB))
+                        spriteA.OnCollide(spriteB);
+                }
+            }
+
+            int count = _sprites.Count;
+            for (int i = 0; i < count; i++)
+            {
+                foreach (var child in _sprites[i].Children)
+                    _sprites.Add(child);
+
+                _sprites[i].Children.Clear();
+            }
+
+            for (int i = 0; i < _sprites.Count; i++)
+            {
+                if (_sprites[i].IsRemoved)
+                {
+                    _sprites.RemoveAt(i);
+                    i--;
+                }
+            }
+        }
+
     }
 }
